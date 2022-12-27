bfedead3b0e954bab8d3264090ed126471225ae2
Luccas <lccs.mrm@gmail.com>
2022-12-27 10:35:44 -0300

    Initial commit

---
+++ src/br/com/fiap/banco/ContaCorrente.java
@@ -0,0 +1,15 @@
+public class ContaCorrente {
+
+    /**
+     * Realiza um depósito na conta corrente
+     * @param agencia O número da agencia
+     * @param numero O número da conta
+     * @param valor O valor que será depositado
+     * @return O número do protocolo de confirmação
+     */
+    public int depositar(int agencia, long numero, double valor){
+        //implementação do método
+        return 0;
+    }
+
+}
